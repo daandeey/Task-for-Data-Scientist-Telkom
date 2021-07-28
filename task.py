90098a24b590b380e1ea640d5333e77bdd6288ce
daandeey <dandyarifrahman@gmail.com>
2021-07-28 21:00:55 +0700

    Updated task.py

--- task.py
+++ task.py
@@ -9,10 +9,12 @@ import pandas as pd
 from os import listdir
 from os.path import join
 
+# Global Variables
 NEW_SIZE = (100, 100)
 DEFAULT_DATA_PATH = 'Data/'
 DEFAULT_CSV_FILENAME = 'images.csv'
 
+# Functions
 def file_to_array(file, image_size=NEW_SIZE):
     # read image as numeric array representation
     image = Image.open(file)
@@ -22,7 +24,8 @@ def file_to_array(file, image_size=NEW_SIZE):
     
     return list(image)
 
-def folder_to_csv(path=DEFAULT_DATA_PATH, filename=DEFAULT_CSV_FILENAME):
+def folder_to_csv(path=DEFAULT_DATA_PATH, filename=DEFAULT_CSV_FILENAME, image_size=NEW_SIZE):
+    # write csv file given folder path with drum and guitar images
     folder = ['drum/', 'guitar/']
     
     # init a data frame
@@ -34,7 +37,7 @@ def folder_to_csv(path=DEFAULT_DATA_PATH, filename=DEFAULT_CSV_FILENAME):
             # inserting label
             df.loc[index, 'label'] = i
             
-            img_arr = file_to_array(join(path, folder[i], file)) # read image as numeric array
+            img_arr = file_to_array(join(path, folder[i], file), image_size=image_size) # read image as numeric array
             img_arr = [str(binary) for binary in img_arr] # convert numeric array to array of string
             img_arr = ','.join(img_arr) # convert array of string to string
             
@@ -48,24 +51,25 @@ def folder_to_csv(path=DEFAULT_DATA_PATH, filename=DEFAULT_CSV_FILENAME):
     df.to_csv(filename, index=False, header=None)
 
 def str_to_array(s):
+    # convert str to numeric array
     return np.asarray([int(binary) for binary in s.split(',')])
 
-def show_img(array, size=NEW_SIZE):
-    img_arr = str_to_array(array).reshape((*size, -1))
+def show_img(array, image_size=NEW_SIZE):
+    # show image of the given string representation of array
+    img_arr = str_to_array(array).reshape((*image_size, -1))
     plt.imshow(img_arr)
     plt.show()
 
 def main():
     # Task 1: write images numeric representation into csv file
     # output file will be DEFAULT_CSV_FILENAME
-    folder_to_csv(path=DEFAULT_DATA_PATH, filename=DEFAULT_CSV_FILENAME)
+    folder_to_csv(path=DEFAULT_DATA_PATH, filename=DEFAULT_CSV_FILENAME, image_size=NEW_SIZE)
     
     # Task 2: display images from csv files
     df = pd.read_csv(DEFAULT_CSV_FILENAME, names=['label', 'array'])
-    for i in range(2):
+    for i in range(len(df)):
         array = df.loc[i, 'array']
-        show_img(array)
+        show_img(array, image_size=NEW_SIZE)
 
 if __name__ == "__main__":
     main()
-
